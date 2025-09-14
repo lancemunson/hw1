783ce9562a81b2f54125ffea3d5eaf162302f6f6
Owner <lmunson@wisc.edu>
2025-09-14 13:52:59 -0500

    Finished HW1

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -1,5 +1,19 @@
 function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
+    
+    const jobListing = document.getElementsByName("job");
+    let noneChecked = true;
 
+    for(let i = 0; i < jobListing.length; i++){
+        if(jobListing[i].checked){
+            alert("Thank you for applying to be a " + jobListing[i].value + "!");
+            noneChecked = false;
+        }
+    }
+
+    if(noneChecked){
+        alert("Please select a job!");
+    }
+    
     // TODO: Alert the user of the job that they applied for!
 }
--- badger-mart/order.js
+++ badger-mart/order.js
@@ -59,15 +59,23 @@ function roundMoney(num) {
 }
 
 function calculateSubtotal() {
-	return 0.00; // TODO calculateSubtotal
+	total = 0;
+	for(let i = 0; i<ITEMS.length; i++){
+		const quantity = parseInt(document.getElementById(ITEMS[i]+"-quantity").value);
+		const price = parseFloat(document.getElementById(ITEMS[i]+"-price").innerText);
+
+		total += quantity*price;
+	}
+	return total; // TODO calculateSubtotal
 }
 
 function calculateSalesTax() {
-	return 0.00; // TODO calculateSalesTax
+	const state = document.getElementById("state-tax").value;
+	return roundMoney(calculateSubtotal()*getSalesTaxRateForState(state)); // TODO calculateSalesTax
 }
 
 function getSalesTaxRateForState(state) {
-	return 0.00; // TODO getSalesTaxRateForState
+	return SALES_TAX[state];
 }
 
 document.getElementById("btn-what-is-my-sales-tax").addEventListener("click", () => {
@@ -81,6 +89,10 @@ document.getElementById("btn-subtotal").addEventListener("click", () => {
 
 document.getElementById("btn-sales-tax").addEventListener("click", () => {
 	alert("Your sales tax is: $" + calculateSalesTax().toFixed(2));
+});
+
+document.getElementById("btn-checkout").addEventListener("click", () => {
+	alert("Your total is: $" + (parseFloat(calculateSalesTax()) + parseFloat(calculateSubtotal().toFixed(2))) );
 });
 
 // TODO Add an event listener to btn-checkout
